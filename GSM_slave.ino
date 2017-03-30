3475028a51396b5d88122dd4ca46256ff8db6476
purnimakam <purnima.kamble@zconsolutions.com>
2017-03-30 14:12:12 +0530

    Add files via upload
    GSM connected to thingspeak

---
+++ GSM_slave.ino
@@ -0,0 +1,103 @@
+#include <SoftwareSerial.h>
+String readString;
+ 
+SoftwareSerial mySerial(7, 8);
+
+void setup()
+{
+  mySerial.begin(9600);               // the GPRS baud rate   
+  Serial.begin(9600);    // the GPRS baud rate 
+
+}
+
+void loop() {
+  int value;
+while(Serial.available())
+{delay(3);
+char c = Serial.read();
+readString+= c;}
+if (readString.length() >0)
+{
+mySerial.print(readString);
+value=readString.toInt();
+mySerial.print("string is");
+mySerial.print(value);
+delay(3000);
+Send2Pachube();
+readString="";
+}
+}
+ void Send2Pachube()
+{
+  mySerial.println("AT");
+  delay(1000);
+
+  mySerial.println("AT+CPIN?");
+  delay(1000);
+
+  mySerial.println("AT+CREG?");
+  delay(1000);
+
+  mySerial.println("AT+CGATT?");
+  delay(1000);
+
+  mySerial.println("AT+CIPSHUT");
+  delay(1000);
+
+  mySerial.println("AT+CIPSTATUS");
+  delay(2000);
+
+  mySerial.println("AT+CIPMUX=0");
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CSTT=\"TATA.DOCOMO.INTERNET\"");//start task and setting the APN,
+  delay(1000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIICR");//bring up wireless connection
+  delay(3000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIFSR");//get local IP adress
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSPRT=0");
+  delay(3000);
+ 
+  ShowSerialData();
+  
+  mySerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
+  delay(6000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSEND");//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+  
+    String str="GET http://api.thingspeak.com/update?api_key=66YIHE946IDI8G41&field1=" + String(readString.toInt());
+  mySerial.println(str);//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+
+  mySerial.println((char)26);//sending
+  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+  mySerial.println();
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSHUT");//close the connection
+  delay(100);
+  ShowSerialData();
+} 
+void ShowSerialData()
+{
+  while(mySerial.available()!=0)
+    Serial.write(mySerial.read());
+}
